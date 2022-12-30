2f9e4ef046a8834f6a9d9266ab8bcfb05433c1ea
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-12-30 16:04:38 +0200

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import Home from './components/Home';
+import Pricing from './components/Pricing';
+import About from './components/About';
+import Contacts from './components/Contacts';
+import NotFound from './components/NotFound';
+import Navigation from './components/Navigation';
+import Starship from './components/Starship';
+import StarshipList from './components/StarshipList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React Router</h1>
+
+      <Navigation />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/pricing/*" element={<Pricing />} />
+        <Route path="/pricing/premium" element={<h2>Premium Pricing</h2>} />
+        <Route path="/contacts" element={<Contacts />} />
+
+        <Route path="/starships" element={<StarshipList />} />
+        <Route path="/starships/:starshipId/*" element={<Starship />} />
+        <Route path="/millennium-falcon" element={<Navigate to="/products/10" replace />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
